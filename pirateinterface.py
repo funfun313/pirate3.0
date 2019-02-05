1ab361f4b96e7f29a71590e292c3ce2b48066548
funfun313 <43829418+funfun313@users.noreply.github.com>
2019-02-04 19:15:13 -0600

    got the selection and arrows working!!! :)

--- pirateinterface.py
+++ pirateinterface.py
@@ -9,6 +9,47 @@ def dofilter(e):
     for pirate in d:
         if filt.lower() in d[pirate]["name"].lower():
             listbox.insert(END, d[pirate]["name"])
+def onselect(e):
+    w = e.widget
+    try:
+        index = int(w.curselection() [0])
+        pirate = w.get(index)
+        #print (pirate, " selected")
+        for p in d:
+            if d[p]["name"].lower() == pirate.lower():
+               display(p)
+    except:
+        pass
+                
+def display(pirate_id):
+    name.config(text =d[pirate_id]["name"])
+    ship.config(text =d[pirate_id]["ship"])
+    if d[pirate_id]["fictional"]== "true":
+        fict.config(text = "Fictional")
+    else:
+        fict.config(text = "Not Fictional")
+def scrollright():
+    index = int(listbox.curselection()[0])
+    listbox.selection_clear(index)
+    if index == len(d)-1:
+        update_listbox(0)
+    else:
+        update_listbox(index + 1)
+def scrollleft():
+    index = int(listbox.curselection()[0])
+    listbox.selection_clear(index)
+    if index == 0:
+        update_listbox(len(d)-1)
+    else:
+        update_listbox(index - 1)
+
+def update_listbox(index):
+    listbox.selection_set(index)
+    piratename = listbox.get(index)
+    for p in d:
+         if d[p]["name"].lower() == piratename.lower():
+               display(p)
+    
 #frame1
 frame1= Frame(window1)
 title =Label(frame1,text="Pirate Database", font="Arial 20")
@@ -25,11 +66,11 @@ searchhere.grid(row=0,column=0)
 frame3= Frame(window1)
 name =Label(frame3, text="Name", font="Arial 20")
 name.grid(row=0,column=1)
-arrow1 =Button(frame3, text="<", font="Arial 20")
+arrow1 =Button(frame3, text="<", font="Arial 20", command = scrollleft)
 arrow1.grid(row=1,column=0)
 pic =Label(frame3, text="picture", font="Arial 16", padx=100)
 pic.grid(row=1,column=1)
-arrow2 =Button(frame3, text=">", font="Arial 20")
+arrow2 =Button(frame3, text=">", font="Arial 20", command = scrollright)
 arrow2.grid(row=1,column=2)
 ship =Label(frame3, text="Ship", font="Arial 16")
 ship.grid(row=2,column=1)
@@ -39,6 +80,7 @@ fict.grid(row=3,column=1)
 frame4= Frame(window1)
 listbox = Listbox(frame4, font="Arial 20")
 listbox.grid(row=0,column=0, columnspan=2)
+listbox.bind("<<ListboxSelect>>", onselect)
     #retrieve pirates from dictionarys
 fm = firebasemanager.FirebaseManager()
 d = fm.getallpirates()
